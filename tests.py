d4832318a65c74052a6c3aec505114c45d84f27a
marcofariasmx <marcofariasmx@gmail.com>
2021-11-22 21:47:03 -0600

    plenty of tests

--- tests.py
+++ tests.py
@@ -1,5 +1,20 @@
 from memory import memory
+import pickle as pickle
 
-memory = memory()
+#memory = memory()
 
-memory.test()
+#memory.test()
+
+myDicts = pickle.load( open ("OBJ.pkl", "rb") )
+
+#1 Global mem
+#2 Constant mem
+#3 Funcs Dir
+#4 Quads 
+for idx, dict in enumerate(myDicts):
+    print("DICT ", idx+1)
+    print(dict)
+
+print("QUADRUPLES: ")
+for idx, operator in enumerate(myDicts[3]['operator']):
+    print(idx+1, ', ', operator, ', ', myDicts[3]['operand1'][idx], ', ', myDicts[3]['operand2'][idx], ', ', myDicts[3]['result'][idx])
